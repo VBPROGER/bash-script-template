f6506bf0a9fe8acf947176222654e851006186d4
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2023-05-01 01:52:22 +0300

    Changed license

--- template.sh
+++ template.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Launch example:
-# #here you can put launch example.
-# #ex: ./mycoolprogram --init --oslinux
+# # Here you can put launch example.
+# # Example: ./mycoolprogram --init --oslinux
 
-NAME=`basename $0`
-VERSION=1.0.0
+NAME=$("basename $0")
+AUTHOR="Your Name Here"
+VERSION="1.0.0"
 DESCRIPTION="
     _._._._._._._._._._._._._._._
     ${NAME} is a simple tool for
@@ -13,25 +14,31 @@ DESCRIPTION="
                   :D
     _._._._._._._._._._._._._._._
 "
-AUTHOR="Your Name"
-LICENSE_YEAR="2022"
-LICENSE_RAW="Copyright ${LICENSE_YEAR} ${AUTHOR}
-Permission is hereby granted, free of charge,
-to any person obtaining a copy of this software
-and associated documentation files (the \"Software\"),
-to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge,
-publish, distribute, sublicense, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished
-to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
+LICENSE="The Unlicense"
+LICENSE_RAW="This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
 THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>"
 
 LOG(){
 echo "${NAME}: ${1}"
@@ -68,25 +75,9 @@ Commands:
 Example:
     ${c}- abc -v def
 License:
-    MIT License
+    ${LICENSE}
 License (description):
-    Copyright ${LICENSE_YEAR} ${AUTHOR}
-    Permission is hereby granted, free of charge,
-    to any person obtaining a copy of this software
-    and associated documentation files (the \"Software\"),
-    to deal in the Software without restriction, including
-    without limitation the rights to use, copy, modify, merge,
-    publish, distribute, sublicense, and/or sell copies of the
-    Software, and to permit persons to whom the Software is furnished
-    to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    ${LICENSE_RAW}
 $ART_NXT
 "
 printf "$HELP_RAW"
